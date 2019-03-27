817205458d9b81af9fc5f5ab19e60d786587f655
Daniel Llatas Spiers <danllatas@gmail.com>
2019-03-27 15:14:33 +0100

    Use looger instead of console

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const teseo = require('teseo');
 const analysis = require('./analysis');
 const synthesis = require('./synthesis');
+const logger = require('./logging');
 
 const main = (schema, flavor, master, name) => {
   // Sort schema
+  logger.info('Schema sorting started.');
   const sortedSchema = teseo.sort(schema, master, name);
-  console.info('Schema sorting finished succesfully.');
+  logger.info('Schema sorting finished succesfully.');
 
   // Execute analysis on schema object
+  logger.info('Schema analysis started.');
   const analyzedSchema = analysis(sortedSchema);
-  console.info('Schema analysis finished succesfully.');
+  logger.info('Schema analysis finished succesfully.');
 
   // Generate SQL code from analyzed schema
+  logger.info('Schema synthesis started.');
   const ddlCode = synthesis(analyzedSchema, flavor);
-  console.info('Schema synthesis finished succesfully.');
+  logger.info('Schema synthesis finished succesfully.');
 
   return ddlCode;
 };
