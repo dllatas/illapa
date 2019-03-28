0dbe7baa4343218527c0e2b62714a267ca0048b8
Daniel Llatas Spiers <danllatas@gmail.com>
2019-03-28 15:12:23 +0100

    Rewrite how errors are reported

--- src/analysis.js
+++ src/analysis.js
@@ -1,102 +1,141 @@
-const indexType = [
-  'unique',
-  'normal',
-];
+let errorList = [];
+
+const indexType = ['unique', 'normal'];
 
 const PROPS_TO_FILTER = ['_migration', 'exported'];
 
-const checkPropName = (name) => {
+const checkPropName = (name, table, prop) => {
   if (!name) {
-    throw new Error('A name must be defined for a column');
+    errorList.push({
+      msg: 'A column must have a name',
+      table: table._table._name,
+      prop,
+    });
   }
 
   if (typeof name !== 'string') {
-    throw new Error('It must have be of type string');
+    errorList.push({
+      msg: `A column name must be of type string. Currently, ${name} is type ${typeof name}`,
+      table: table._table._name,
+      prop,
+    });
   }
 };
 
-const checkPropType = (type) => {
+const checkPropType = (type, table, prop) => {
   if (!type) {
-    throw new Error('A type must be defined for a column');
+    errorList.push({
+      msg: 'A type must be defined for a column',
+      table: table._table._name,
+      prop,
+    });
   }
 
   if (typeof type !== 'string') {
-    throw new Error('It must have be of type string');
+    errorList.push({
+      msg: `A name must be of type string. Currently, ${type} is type ${typeof type}`,
+      table: table._table._name,
+      prop,
+    });
   }
 };
 
-const checkPropSize = (size) => {
-  if (typeof size === 'string') {
-    size = size.replace(',', '.');
+const checkPropSize = (size, table, prop) => {
+  let sSize = String(size);
+
+  // Check if sSize can be parsed into a number
+  let formatForNumber = sSize;
+  formatForNumber = formatForNumber.replace(',', '.');
+  const parsedSize = Number(formatForNumber);
+  if (Number.isNaN(parsedSize)) {
+    errorList.push({
+      msg: `Column size must be a valid number representation. Currently, ${parsedSize}`,
+      table: table._table._name,
+      prop,
+    });
   }
 
-  if (typeof size !== 'number') {
-    const parsedSize = Number(size);
-    if (Number.isNaN(parsedSize)) {
-      throw new Error('The size of a column must be of type number');
-    }
-  }
+  // Replace the dots by commas
+  sSize = sSize.replace('.', ',');
+  return sSize;
 };
 
-const checkPropDefaultValue = (defaultValue) => {
+const checkPropDefaultValue = (defaultValue, table, prop) => {
   if (defaultValue && typeof defaultValue === 'string' && defaultValue !== 'NULL') {
     return `'${defaultValue}'`;
   }
   return defaultValue;
 };
 
-const checkColumn = (column) => {
+const checkColumn = (column, table, prop) => {
   const checkedColumn = column.map((c) => {
-    checkPropName(c._name);
-    checkPropType(c._type);
-    checkPropSize(c._length);
-    c._default = checkPropDefaultValue(c._default);
+    checkPropName(c._name, table, prop);
+    checkPropType(c._type, table, prop);
+    c._length = checkPropSize(c._length, table, prop);
+    c._default = checkPropDefaultValue(c._default, table, prop);
     return c;
   });
   return checkedColumn;
 };
 
-const checkPrimary = (value, table) => {
-  // Check that value if an array
+const checkPrimary = (value, table, prop) => {
   if (!Array.isArray(value)) {
-    throw new Error('Primary key should be an array');
+    value = [value];
   }
 
-  // Check that all elements in primary array exist on column array
   const columnNames = table._column.map(c => c._name);
 
   for (const v of value) {
     if (!columnNames.includes(v)) {
-      throw new Error(`The primary key should be a column on the table: ${v} is not one`);
+      errorList.push({
+        msg: `The primary key should be a column on the table: ${v} is not one`,
+        table: table._table._name,
+        prop,
+      });
     }
   }
 
   return value;
 };
 
-const checkIndex = (value, table) => {
+const checkIndex = (value, table, prop) => {
   const columnNames = table._column.map(c => c._name);
 
   // Check that array is not null
   if (!Array.isArray(value)) {
-    throw new Error('Index should be an array');
+    value = [value];
   }
 
   for (const v of value) {
     // Check if name is string
     if (typeof v._name !== 'string') {
-      throw new Error(`The index name should be a string: ${v._name} (${typeof v._name})`);
+      errorList.push({
+        msg: `The index name should be a string: ${v._name} (${typeof v._name})`,
+        table: table._table._name,
+        prop,
+        name: v._name,
+      });
     }
 
     // Check that index type is legal
     if (!indexType.includes(v._type)) {
-      throw new Error(`The index type is not defined: ${v._type}`);
+      errorList.push({
+        msg: `The index type is not defined: ${v._type}`,
+        table: table._table._name,
+        prop,
+        name: v._name,
+      });
     }
 
     // Check that all elements in column exist on column array
     for (const c of v._column) {
       if (!columnNames.includes(c)) {
-        throw new Error(`The index column should be a column on the table: ${c} is not one`);
+        errorList.push({
+          msg: `The index column should be a column on the table: ${c} is not one`,
+          table: table._table._name,
+          prop,
+          name: v._name,
+        });
       }
     }
   }
@@ -104,9 +143,13 @@ const checkIndex = (value, table) => {
   return value;
 };
 
-const checkTable = (value) => {
+const checkTable = (value, table, prop) => {
   if (typeof value._name !== 'string') {
-    throw new Error(`The table name must be a string. Instead table ${value._name} has a name of type ${typeof value._name}`);
+    errorList.push({
+      msg: `The table name must be a string. Instead table ${value._name} has a name of type ${typeof value._name}`,
+      table: table._table._name,
+      prop,
+    });
   }
 
   if (!value._force) {
@@ -115,7 +158,11 @@ const checkTable = (value) => {
   }
 
   if (typeof value._force !== 'boolean') {
-    throw new Error(`The table force must be a boolean. Instead table ${value._name} has a force of type ${typeof value._force}`);
+    errorList.push({
+      msg: `The table force must be a boolean. Instead table ${value._name} has a force of type ${typeof value._force}`,
+      table: table._table._name,
+      prop,
+    });
   }
 
   return value;
@@ -124,14 +171,15 @@ const checkTable = (value) => {
 const checkForeign = value => value;
 
 const dispatcher = {
-  _table: (value, table) => checkTable(value, table),
-  _column: (value, table) => checkColumn(value, table),
-  _primary: (value, table) => checkPrimary(value, table),
-  _index: (value, table) => checkIndex(value, table),
-  _foreign: (value, table) => checkForeign(value, table),
+  _table: (value, table, prop) => checkTable(value, table, prop),
+  _column: (value, table, prop) => checkColumn(value, table, prop),
+  _primary: (value, table, prop) => checkPrimary(value, table, prop),
+  _index: (value, table, prop) => checkIndex(value, table, prop),
+  _foreign: (value, table, prop) => checkForeign(value, table, prop),
 };
 
 const analysis = (schema) => {
+  errorList = [];
   const analyzedSchema = [];
 
   if (!Array.isArray(schema)) {
@@ -145,7 +193,7 @@ const analysis = (schema) => {
     const props = Object.keys(table).filter(k => !PROPS_TO_FILTER.includes(k));
 
     for (let j = 0; j < props.length; j += 1) {
-      const analyzedProp = dispatcher[props[j]](table[props[j]], table);
+      const analyzedProp = dispatcher[props[j]](table[props[j]], table, props[j]);
       if (analyzedProp) {
         analyzedTable[props[j]] = analyzedProp;
       }
@@ -153,7 +201,10 @@ const analysis = (schema) => {
     analyzedSchema.push(analyzedTable);
   }
 
-  return analyzedSchema;
+  return {
+    schema: analyzedSchema,
+    errorList,
+  };
 };
 
 module.exports = analysis;
